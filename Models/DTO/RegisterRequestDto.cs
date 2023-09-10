7d6d4d7e7c354c08fc1a5e0700b8faf8cdf783ba
RANN1D1901 <navdeeprandhawa0001@gmail.com>
2023-09-10 18:54:20 +1000

    Add auth controller and register endpoint along with logic to register user using UserManager class.

---
+++ Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NSWalks.API.Models.DTO
+{
+	public class RegisterRequestDto
+	{
+		public RegisterRequestDto()
+		{
+		}
+        [Required]
+		[DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		public string[] Roles { get; set; }
+    }
+}
+
